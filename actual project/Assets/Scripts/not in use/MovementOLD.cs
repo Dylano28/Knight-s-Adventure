510ffd1338cdb6f389bddea0a86435dd3f29c256
Dylano28 <102656781+Dylano28@users.noreply.github.com>
2024-10-24 15:44:28 +0200

    latest trends

---
+++ actual project/Assets/Scripts/not in use/MovementOLD.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    private float moveInput;
+    private Rigidbody2D rb;
+    public float jumpForce;
+    public bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+    public GameObject platform;
+    public Animator animator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (moveInput > 0)
+        {
+            gameObject.transform.localScale = new Vector3(2, 2, 2);
+        }
+
+        if (moveInput < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-2, 2, 2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        animator.SetFloat("Speed", Mathf.Abs(moveInput)); 
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            animator.SetBool("IsJumping", true);
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+                animator.SetBool("IsJumping", false);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+    }
+}
